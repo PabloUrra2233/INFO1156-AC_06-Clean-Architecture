2d49cf5aacd37912a52013bf11b1639df8ae8eff
Eduardo Necul <enecul2025@alu.uct.cl>
2026-06-04 21:55:13 -0400

    feat: define posts domain entities and repository contract
    Es únicamente la estructura, su función es que tengamos los cimientos de manera igual para nuestros avances del código. Esto no esta conectado al código actualmente, aunque el objetivo es tecnicamente hacerlo.

---
+++ src/posts/domain/repositories/post.repository.interface.ts
@@ -0,0 +1,18 @@
+import { PostEntity, PostFeedEntity } from '../entities/post.entity';
+
+export const POST_REPOSITORY_TOKEN = Symbol('IPostRepository');
+
+export interface IPostRepository {
+    create(data: { 
+        title: string; 
+        description: string; 
+        imageUrl: string; 
+        categoryId?: string 
+    }): Promise<PostEntity>;
+    
+    findById(id: string): Promise<PostEntity | null>;
+    
+    findAll(): Promise<PostEntity[]>;
+    
+    getFeedPosts(categoryId?: string): Promise<PostFeedEntity[]>;
+}
